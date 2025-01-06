a98e3d64988fd76bb946550bd01c716de26b1764
Morgan J. Lopes <morgan@polarnotion.com>
2025-01-06 10:09:25 -1000

    Add custom domain to production.rb environments.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
+  config.hosts << ".launchwithless.com"
+
   # Eager load code on boot for better performance and memory savings (ignored by Rake tasks).
   config.eager_load = true
 
