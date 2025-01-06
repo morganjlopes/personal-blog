0e81e47a7f1b205f78ba3f48be586ad03c0b3134
Morgan J. Lopes <morgan@polarnotion.com>
2025-01-06 10:35:45 -1000

    Create temporary fix for subdomain issues.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  before_action :_set_domain
+  # before_action :_set_domain
 
   def _set_domain
     unless request.domain == 'launchwithless.com'
--- app/controllers/public/pages_controller.rb
+++ app/controllers/public/pages_controller.rb
@@ -2,6 +2,6 @@ class Public::PagesController < ApplicationController
   def home
     @website = Website.all.first
 
-    redirect_to root_url(subdomain: @website.subdomain), allow_other_host: true if @website.present?
+    # redirect_to root_url(subdomain: @website.subdomain), allow_other_host: true if @website.present?
   end
 end
