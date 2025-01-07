748b6e7da9d30764cf9b2770b4394e462d44c5ea
Morgan J. Lopes <morgan@polarnotion.com>
2025-01-06 17:56:36 -1000

    Enable subdomain check.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  # before_action :_set_domain
+  before_action :_set_domain
 
   def _set_domain
-    unless request.domain == 'launchwithless.com'
+    unless request.subdomain == 'www'
       @subdomain = request.subdomain
       @website   = Website.find_by(subdomain: @subdomain)
     end
