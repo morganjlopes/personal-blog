7c8965f0d7f5012ba15fb5ea2272e3ea1363467f
Morgan J. Lopes <morgan@polarnotion.com>
2025-01-06 15:00:34 -1000

    Update procfile.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[8.0].define(version: 2025_01_06_193237) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+  enable_extension "pg_stat_statements"
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
